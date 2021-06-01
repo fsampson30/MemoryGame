b78060df059b4b2be97a86ad9c677d106bec293c
fsampson30@ <fsampson30@gmail.com>
2021-06-01 15:57:26 -0300

    UI adjustments Text moves logic created Pairs found logic created Buttons Refresh Game and Choose Board SIze Created

--- app/src/main/java/com/sampson/memorygame/MainActivity.kt
+++ app/src/main/java/com/sampson/memorygame/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.sampson.memorygame
 
+import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.snackbar.Snackbar
@@ -39,8 +47,84 @@ class MainActivity : AppCompatActivity() {
         txtMoves = findViewById(R.id.txtMoves)
         txtPairs = findViewById(R.id.txtPairs)
 
-        memoryGame = MemoryGame(boardSize)
+        txtPairs.setTextColor(ContextCompat.getColor(this, R.color.color_progress_none))
+        setupBoard()
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.mi_refresh -> {
+                if (memoryGame.getNumMoves() > 0 && !memoryGame.haveWonGame()) {
+                    showAlertDialog("Quit your current game?", null, View.OnClickListener {
+                        setupBoard()
+                    })
+                } else {
+                    setupBoard()
+                }
+                return true
+            }
+            R.id.mi_new_size -> {
+                showNewSizeDialog()
+                return true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showNewSizeDialog() {
+        val boardSizeView = LayoutInflater.from(this).inflate(R.layout.dialog_board_size, null)
+        val radioGroupSize = boardSizeView.findViewById<RadioGroup>(R.id.radioGroupBoardSize)
+        when(boardSize){
+            BoardSize.EASY -> radioGroupSize.check(R.id.rbEasy)
+            BoardSize.MEDIUM -> radioGroupSize.check(R.id.rbMedium)
+            BoardSize.HARD -> radioGroupSize.check(R.id.rbHard)
+        }
+        showAlertDialog("Choose new Size", boardSizeView, View.OnClickListener {
+            boardSize = when (radioGroupSize.checkedRadioButtonId) {
+                R.id.rbEasy -> BoardSize.EASY
+                R.id.rbMedium -> BoardSize.MEDIUM
+                else -> BoardSize.HARD
+            }
+            setupBoard()
+        })
+    }
+
+    private fun showAlertDialog(
+        title: String,
+        view: View?,
+        positiveClickListener: View.OnClickListener
+    ) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setView(view)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK") { _, _ ->
+                positiveClickListener.onClick(null)
+            }.show()
+    }
+
+    private fun setupBoard() {
+        when (boardSize) {
+            BoardSize.EASY -> {
+                txtMoves.text = "Easy: 4 x 2"
+                txtPairs.text = "Pairs: 0 / 4"
+            }
+            BoardSize.MEDIUM -> {
+                txtMoves.text = "Medium: 6 x 3"
+                txtPairs.text = "Pairs: 0 / 9"
+            }
+            BoardSize.HARD -> {
+                txtMoves.text = "Hard: 6 x 4"
+                txtPairs.text = "Pairs: 0 / 12"
+            }
+        }
+        memoryGame = MemoryGame(boardSize)
         adapter = MemoryBoardAdapter(
             this,
             boardSize,
@@ -55,8 +139,8 @@ class MainActivity : AppCompatActivity() {
         recyclerBoard.adapter = adapter
         recyclerBoard.setHasFixedSize(true)
         recyclerBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
-
     }
+
 
     private fun updateGameWithFlip(position: Int) {
         if (memoryGame.haveWonGame()) {
@@ -64,12 +148,24 @@ class MainActivity : AppCompatActivity() {
             return
         }
         if (memoryGame.isCardFaceUp(position)) {
-            Snackbar.make(clRoot, "Invalid movement!", Snackbar.LENGTH_LONG).show()
+            Snackbar.make(clRoot, "Invalid movement!", Snackbar.LENGTH_SHORT).show()
             return
         }
-        if (memoryGame.flipCard(position)){
-            Log.i(TAG,"Found a match! Num Pairs found: ${memoryGame.numPairsFound}")
+        if (memoryGame.flipCard(position)) {
+            Log.i(TAG, "Found a match! Num Pairs found: ${memoryGame.numPairsFound}")
+            val color = ArgbEvaluator().evaluate(
+                memoryGame.numPairsFound.toFloat() / boardSize.getNumPairs(),
+                ContextCompat.getColor(this, R.color.color_progress_none),
+                ContextCompat.getColor(this, R.color.color_progress_full)
+
+            ) as Int
+            txtPairs.setTextColor(color)
+            txtPairs.text = "Pairs: ${memoryGame.numPairsFound} / ${boardSize.getNumPairs()} "
+            if (memoryGame.haveWonGame()) {
+                Snackbar.make(clRoot, "You won! Congratulations.", Snackbar.LENGTH_LONG).show()
+            }
         }
+        txtMoves.text = "Moves: ${memoryGame.getNumMoves()}"
         adapter.notifyDataSetChanged()
 
     }
